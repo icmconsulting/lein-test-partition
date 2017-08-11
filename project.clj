9d65afa1e70570f548cc3c094e34e9fb4037f6e8
Brendan Bates <batesbrendan@gmail.com>
2017-08-11 15:31:35 +1000

    Make snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-test-partition "1.0.0"
+(defproject lein-test-partition "1.0.1-SNAPSHOT"
   :description "Partition your clojure.test test suite across multiple JVMs."
   :url "https://github.com/icmconsulting/lein-test-partition"
   :license {:name "Eclipse Public License"
