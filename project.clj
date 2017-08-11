a4cf66d8cf538a6fb0a8fefd518a97309844210e
Brendan Bates <batesbrendan@gmail.com>
2017-08-11 15:41:59 +1000

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-test-partition "1.0.1"
+(defproject lein-test-partition "1.0.2-SNAPSHOT"
   :description "Partition your clojure.test test suite across multiple JVMs."
   :url "https://github.com/icmconsulting/lein-test-partition"
   :license {:name "Eclipse Public License"
